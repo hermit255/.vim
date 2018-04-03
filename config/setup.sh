a701fb8821046b81768fa42522615fe67736f62d
hermit255 <hermit255@gmail.com>
2018-04-03 10:41:18 +0900

    Update setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -1,9 +1,11 @@
-echo "creating .vimrc in home directory..."
+echo "Copying .vimrc in home directory..."
 cp ~/.vim/config/.vimrc ~/
-echo "creating backup directory..."
+echo "Creating backup directory..."
 mkdir -p ~/.backup
-echo "creating dein directory..."
+echo "Creating dein directory..."
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-echo "cloning dein.vim..."
+echo "Cloning dein.vim..."
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
-echo "finished"
+echo "Installing plugins with dein..."
+vim -c call "dein#install()" -c "q!"
+echo "Finished!! Now you can use vim with your setting"
