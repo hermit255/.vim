ad0119f788b621a8516e35437ef807acf8229e60
hermit255 <hermit255@gmail.com>
2018-07-22 11:40:00 +0900

    sh が間違っていた

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cp .vimrc ../
 # echo " Cloning dein.vim..."
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./.working/installer.sh
 echo " Installing dein.vim..."
-sh ./.working/installer.sh ./.cache/dein
+sh ./.working/installer.sh ./.working/dein
 # echo "Setting vimdiff as difftool and mergetool..."
 # git config --global diff.tool vimdiff
 # git config --global difftool.prompt false
